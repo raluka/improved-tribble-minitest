a636f19eb503526cbc8c4b0e09295de13e4935b3
raluca badoi <badoi.raluca@gmail.com>
2022-03-20 19:39:03 +0100

    Refactoring Tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,20 +3,23 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(name: 'John', email: 'john@example.com')
+  end
+
   test 'valid user' do
-    user = User.new(name: 'John', email: 'john@example.com')
-    assert user.valid?
+    assert @user.valid?
   end
 
   test 'invalid without name' do
-    user = User.new(email: 'john@example.com')
-    refute user.valid?, 'user is valid without name'
-    assert_not_nil user.errors[:name], 'no validation error for name present'
+    @user.name = nil
+    refute @user.valid?, 'user is valid without name'
+    assert_not_nil @user.errors[:name], 'no validation error for name present'
   end
 
   test 'invalid without email' do
-    user = User.new(name: 'John')
-    refute user.valid?
-    assert_not_nil user.errors[:email]
+    @user.email = nil
+    refute @user.valid?
+    assert_not_nil @user.errors[:email]
   end
 end
